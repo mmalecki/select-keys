303b58e987bf6bc096c20fe74b720f1f6fb202be
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-07-18 02:17:31 +0200

    [fix] Do not copy keys which do not exist

--- test/select-keys-test.js
+++ test/select-keys-test.js
@@ -2,3 +2,4 @@ var assert = require('assert'),
     selectKeys = require('../');
 
 assert.deepEqual(selectKeys({ a: 42, b: 'foo', c: 1337 }, ['a', 'c']), { a: 42, c: 1337 });
+assert.deepEqual(selectKeys({ a: 42, b: 'foo', c: 1337 }, ['a', 'd']), { a: 42 });
